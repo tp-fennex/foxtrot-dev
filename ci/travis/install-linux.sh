1e7ebb4accedbbbf8427591f29665b0c22ab7904
navarka.all@gmail.com <navarka.all@gmail.com>
2019-05-19 21:13:15 +0300

    Latest SFML for build

--- ci/travis/install-linux.sh
+++ ci/travis/install-linux.sh
@@ -2,4 +2,27 @@
 
 # Dependency install script
 sudo apt update
-sudo apt install -y protobuf-compiler libboost-all-dev libsfml-dev libprotobuf-dev libprotoc-dev
+sudo apt install -y protobuf-compiler \
+                    libboost-all-dev \
+                    libprotobuf-dev \
+                    libprotoc-dev
+
+# Build and install latest SFML
+sudo apt install -y libfreetype-dev \
+                    libx11-dev \
+                    libxrandr-dev \
+                    libudev-dev \
+                    libglu1-mesa-dev \
+                    freeglut3-dev \
+                    mesa-common-dev \
+                    libflac-dev \
+                    libogg-dev \
+                    libvorbis-dev \
+                    libvorbisenc2 \
+                    libvorbisfile3 \
+                    libopenal-dev \
+                    libpthread-stubs0-dev
+cd foxtrot/vendor/sfml
+cmake .
+make
+sudo make install
