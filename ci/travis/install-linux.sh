3d32ae172c93180d626f04ebd63197d7d2c7f75d
navarka.all@gmail.com <navarka.all@gmail.com>
2019-05-26 20:42:25 +0300

    CI update

--- ci/travis/install-linux.sh
+++ ci/travis/install-linux.sh
@@ -7,26 +7,44 @@
 #                    libprotobuf-dev \
 #                    libprotoc-dev
 #
-## Build and install latest SFML
-#sudo apt install -y libfreetype6-dev \
-#                    libx11-dev \
-#                    libxrandr-dev \
-#                    libudev-dev \
-#                    libglu1-mesa-dev \
-#                    freeglut3-dev \
-#                    mesa-common-dev \
-#                    libflac-dev \
-#                    libogg-dev \
-#                    libvorbis-dev \
-#                    libvorbisenc2 \
-#                    libvorbisfile3 \
-#                    libopenal-dev \
-#                    libpthread-stubs0-dev
+# Build and install latest SFML
+sudo apt install -y libfreetype6-dev \
+                    libx11-dev \
+                    libxrandr-dev \
+                    libudev-dev \
+                    libglu1-mesa-dev \
+                    freeglut3-dev \
+                    mesa-common-dev \
+                    libflac-dev \
+                    libogg-dev \
+                    libvorbis-dev \
+                    libvorbisenc2 \
+                    libvorbisfile3 \
+                    libopenal-dev \
+                    libpthread-stubs0-dev
 cd foxtrot/vendor/sfml
 cmake .
 make
 sudo make install
 cd ../../..
+
+sudo apt install -y autoconf \
+                    automake \
+                    libtool \
+                    curl \
+                    make \
+                    unzip
+
+git clone https://github.com/protocolbuffers/protobuf.git
+cd protobuf
+git submodule update --init --recursive
+./autogen.sh
+./configure
+make
+make check
+sudo make install
+sudo ldconfig
+cd ..
 
 cd foxtrot/include/foxtrot/Event/proto
 chmod a+x generate.sh
