043b7b44ee1f6db8f08b064d96e192198e309c32
navarka.all@gmail.com <navarka.all@gmail.com>
2019-05-26 20:28:34 +0300

    CI update

--- ci/travis/install-linux.sh
+++ ci/travis/install-linux.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
 # Dependency install script
-sudo apt update
-sudo apt install -y protobuf-compiler \
-                    libboost-all-dev \
-                    libprotobuf-dev \
-                    libprotoc-dev
-
-# Build and install latest SFML
-sudo apt install -y libfreetype6-dev \
-                    libx11-dev \
-                    libxrandr-dev \
-                    libudev-dev \
-                    libglu1-mesa-dev \
-                    freeglut3-dev \
-                    mesa-common-dev \
-                    libflac-dev \
-                    libogg-dev \
-                    libvorbis-dev \
-                    libvorbisenc2 \
-                    libvorbisfile3 \
-                    libopenal-dev \
-                    libpthread-stubs0-dev
+#sudo apt update
+#sudo apt install -y protobuf-compiler \
+#                    libboost-all-dev \
+#                    libprotobuf-dev \
+#                    libprotoc-dev
+#
+## Build and install latest SFML
+#sudo apt install -y libfreetype6-dev \
+#                    libx11-dev \
+#                    libxrandr-dev \
+#                    libudev-dev \
+#                    libglu1-mesa-dev \
+#                    freeglut3-dev \
+#                    mesa-common-dev \
+#                    libflac-dev \
+#                    libogg-dev \
+#                    libvorbis-dev \
+#                    libvorbisenc2 \
+#                    libvorbisfile3 \
+#                    libopenal-dev \
+#                    libpthread-stubs0-dev
 cd foxtrot/vendor/sfml
 cmake .
 make
