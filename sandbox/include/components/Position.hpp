639b44796549a8c4cf50cb9bb6c185f5083a89ea
Babich <g8h2.babayan@yandex.ru>
2019-05-26 18:22:42 +0300

    Some custom components and factories

---
+++ sandbox/include/components/Position.hpp
@@ -0,0 +1,10 @@
+#ifndef COMPONENT_POSITION_HPP
+#define COMPONENT_POSITION_HPP
+
+struct Position
+{
+    int x = 0;
+    int y = 0;
+};
+
+#endif // COMPONENT_POSITION_HPP
---
+++ sandbox/include/core/factories.hpp
@@ -0,0 +1,11 @@
+#ifndef CORE_FACTORIES_HPP
+#define CORE_FACTORIES_HPP
+
+
+#include <entt/entity/registry.hpp>
+#include <SFML/Graphics.hpp>
+
+
+entt::entity make_player(entt::registry& registry);
+
+#endif // CORE_FACTORIES_HPP
